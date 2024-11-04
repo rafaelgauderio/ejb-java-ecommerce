429f8dbfc55682efb5921e331be0a1aff9928251
Rafael de Luca <deluca1712@gmail.com>
2024-11-04 17:03:50 -0300

    jsf cdi bean method add to cart

---
+++ src/main/java/br/jsf/JsfPurchase.java
@@ -0,0 +1,36 @@
+
+package br.jsf;
+
+import br.data.model.ItemPurchase;
+import br.data.model.Product;
+import br.ejb.EjbPurchase;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.ArrayList;
+import javax.ejb.EJB;
+
+/**
+ *
+ * @author Rafael de Luac
+ */
+@Named(value = "jsfPurchase")
+@SessionScoped
+public class JsfPurchase implements Serializable {
+
+    @EJB
+    private EjbPurchase ejbPurchase;    
+    
+    public JsfPurchase() {
+                
+    }
+    
+    public void addProduct(Product product) {
+        ejbPurchase.addProduct(product);
+    }
+    
+    public ArrayList<ItemPurchase> getAllProductsOnCart () {
+        return ejbPurchase.getAllProductOnCart();
+    }
+    
+}
